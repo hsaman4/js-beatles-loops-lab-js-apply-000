2cfb97a88b632692c6ecb02df1aea52487ecbab3
generic-callback-1830 <hadeeqasaman@yahoo.com>
2019-08-26 17:54:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function theBeatlesPlay(musicians, instruments) {
   return arr; 
 }
 
-function johnLennonFacts () {
-  var facts = [
+function johnLennonFacts (facts) {
+  facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
