0923a8d74ce308916dac1915e3effc4067973854
generic-callback-1830 <hadeeqasaman@yahoo.com>
2019-08-26 17:59:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function johnLennonFacts (facts) {
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice" ] ; 
-  const newOne = []; 
-  let x = 0 ;
+  var newOne = []; 
+  var x = 0 ;
 while (x < facts.length){
   newOne.push(`${facts[x]}!!!`) ;
-  x++:
+  x++
 }
 return newOne; 
 }
