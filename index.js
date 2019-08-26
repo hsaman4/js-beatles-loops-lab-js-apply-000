a8eee171202ca3a01acf57ab1d26cb4c8195e8b6
generic-callback-1830 <hadeeqasaman@yahoo.com>
2019-08-26 17:43:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for (let x=0;x < musicians.length; x++){
+    var string = `${musicians[x]} plays ${instruments[x]}`;
+    arr.push(string);
+  }
+  return arr; 
+}
+
+function johnLennonFacts () {
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice" ] ; 
+
+var newOne = []; 
+var x = 0 
+while (x < facts.length)
+  newOne.push(facts [x] + "!!!") ;
+  x++
+return newOne; 
+
+
+
+}
